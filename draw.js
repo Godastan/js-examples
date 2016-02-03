66b38105ea322b7039bf5e4180719a2f2e3739de
godastan <godastan@gmail.com>
2016-02-03 21:57:16 +0300

    drawing tree

--- draw.js
+++ draw.js
@@ -10,38 +10,55 @@ function draw(tree) {
     var canvas = document.getElementById("test_canvas");
     var ctx = canvas.getContext('2d');
 
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawChildren(ctx, root, colWidth, startX, startY, color);
 
     function drawChildren(ctx, node, colWidth, x, y, color) {
-        drawNode(ctx, node, x, y, color);
+
+        var nextLevelY = y + levelHeight;
 
         if (node.getLeftChild() !== null) {
+            drawLine(ctx, x, y, x - colWidth / 4, nextLevelY);
             drawChildren(
                 ctx,
                 node.getLeftChild(),
                 colWidth / 2,
                 x - colWidth / 4,
-                y + levelHeight,
+                nextLevelY,
                 color
             );
         }
 
         if (node.getRightChild()) {
+            drawLine(ctx, x, y, x + colWidth / 4, nextLevelY);
             drawChildren(
                 ctx,
                 node.getRightChild(),
                 colWidth / 2,
                 x + colWidth / 4,
-                y + levelHeight,
+                nextLevelY,
                 color
             );
         }
+
+        drawNode(ctx, node, x, y, color);
     }
 
     function drawNode(ctx, node, x, y, color) {
-        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI);
         ctx.fill();
+        ctx.font='13pt Calibri';
+        ctx.fillStyle='black';
+        ctx.fillText(node.getKey(), x - radius / 2, y);
+    }
+
+    function drawLine(ctx, x1, y1, x2, y2){
+        ctx.beginPath();
+        ctx.moveTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        ctx.stroke();
+        ctx.closePath();
     }
 }
