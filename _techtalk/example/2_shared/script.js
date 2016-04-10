66e98d5e40131147e70fe0506adfee03d788a95b
Daniil <d4n1k@ya.ru>
2016-04-11 00:18:28 +0300

    techtalk files

--- _techtalk/example/2_shared/script.js
+++ _techtalk/example/2_shared/script.js
@@ -1,20 +1,37 @@
-/*
- * WORKER #1
- *
- */
-
 var result = document.getElementById('result');
+var connectionId;
 
 var worker = new SharedWorker('shared_worker.js');
-console.log(worker);
 worker.port.addEventListener('message', function (e) {
-    console.log(e);
-    result.innerHTML += e.data + '<br>';
+    switch (e.data.type){
+        case 'connection':
+            connectionId = e.data.connection_id;
+            result.innerHTML += `Connection ID: ${connectionId}<br>`;
+            break;
+        case 'message':
+            result.innerHTML += `Message from ${e.data.sender}: ${e.data.message}<br>`;
+            break;
+        default:
+            break;
+    }
 });
 worker.port.start();
 
 var btn = document.querySelector('#btn');
 btn.addEventListener('click', function () {
-    console.log('page 1 sent a msg');
-    worker.port.postMessage('request_1');
+    let receiverId = document.getElementById('receiver').value;
+    let messageText = document.getElementById('message').value;
+    let message = {
+        type: 'message',
+        sender: connectionId,
+        receiver: receiverId,
+        message: messageText
+    };
+    result.innerHTML += `Message to ${message.receiver}: ${message.message}<br>`;
+    worker.port.postMessage(message);
+});
+
+var closeBtn = document.getElementById('close_btn');
+closeBtn.addEventListener('click', function () {
+   worker.terminate();
 });
--- _techtalk/example/2_shared/shared_worker.js
+++ _techtalk/example/2_shared/shared_worker.js
@@ -1,12 +1,27 @@
 var connectionsCount = 0;
-
+var allPorts = {};
 self.addEventListener('connect', function (e) {
     var port = e.ports[0];
     connectionsCount++;
-    port.postMessage(`Connection number: ${connectionsCount}`);
+    allPorts[connectionsCount] = port;
+
+    port.postMessage({
+        type: 'connection',
+        connection_id: connectionsCount
+    });
 
     port.addEventListener('message', function (e) {
-        port.postMessage(`connections: ${connectionsCount} | data : ${e.data}`);
+        switch (e.data.type){
+            case 'message':
+                allPorts[e.data.receiver].postMessage({
+                    type: 'message',
+                    message: e.data.message,
+                    sender: e.data.sender
+                });
+                break;
+            default:
+                break;
+        }
     });
 
     port.start();
